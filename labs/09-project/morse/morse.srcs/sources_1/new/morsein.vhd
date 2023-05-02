d7423a1aaf01f11351a644c6ea095569ed925c9a
Michal Hanus <hanus.michal@divelit.cz>
2023-05-02 15:49:30 +0200

    REDME and code comments added

--- labs/09-project/morse/morse.srcs/sources_1/new/morsein.vhd
+++ labs/09-project/morse/morse.srcs/sources_1/new/morsein.vhd
@@ -1,15 +1,5 @@
 ----------------------------------------------------------
---
---! @title Driver for 4-digit 7-segment display
---! @author Tomas Fryza
---! Dept. of Radio Electronics, Brno Univ. of Technology, Czechia
---!
---! @copyright (c) 2020 Tomas Fryza
---! This work is licensed under the terms of the MIT license
---
--- Hardware: Nexys A7-50T, xc7a50ticsg324-1L
--- Software: TerosHDL, Vivado 2020.2, EDA Playground
---
+-- when morse code appear on input pin, record it and save to 'tim'
 ----------------------------------------------------------
 
 library ieee;
@@ -17,29 +7,15 @@ library ieee;
   use ieee.numeric_std.all;
 
 ----------------------------------------------------------
--- Entity declaration for display driver
---
---             +-------------------+
---        -----|> clk              |
---        -----| rst            dp |-----
---             |          seg(6:0) |--/--
---        --/--| data0(3:0)        |  7
---        --/--| data1(3:0)        |
---        --/--| data2(3:0)        |
---        --/--| data3(3:0)        |
---          4  |           dig(3:0)|--/--
---        --/--| dp_vect(3:0)      |  4
---          4  +-------------------+
 --
 -- Inputs:
---  tim(22:0):          -- Morse timing with added zeros at the end
 --  clke:          -- Main clock from clock enable
---  rst:          -- Asynchronous reset
---  start:        -- signal to start decoding
+--  inp:           -- pin with morse code 
+--  rst            -- Async reset
 --
 -- Outputs:
---   outp:          -- morse timing without zeros at the end
---   proc:     -- signal showing whether entity is processing or not
+--   tim(22:0):     -- Morse timing with added zeros at the end
+--   proc:          -- signal showing whether entity is processing or not (RGB LED)
 --   
 --
 ----------------------------------------------------------
@@ -55,7 +31,7 @@ entity morsein is
 end entity morsein;
 
 ----------------------------------------------------------
--- Architecture declaration for display driver
+-- Architecture declaration 
 ----------------------------------------------------------
 
 architecture behavioral of morsein is
@@ -68,28 +44,27 @@ architecture behavioral of morsein is
 
 begin
 
-  --------------------------------------------------------
-  -- p_traffic_fsm:
-  -- A sequential process with synchronous reset and
-  -- clock_enable entirely controls the s_state signal by
-  -- CASE statement.
-  --------------------------------------------------------
   p_morse_in : process (clke) is
   begin
     if rst = '0' then
         if (rising_edge(clke)) then
             case sig_state is
               when 0 =>
+                -- start reset all, wait for input
+
                 tim <= "0000000000000000000000";
                 proc <= "111";
                 sig_cnt <= 20;
                 sig_zeros_cnt <= 0;
-                if inp = '1' then
+
+                if inp = '1' then -- recording start -> save current value and start recording 
                     tim(21) <= '1';
                     sig_state <= 1;
                 end if;
                 
               when 1 =>
+                -- record timing, move to waiting state when 3 noninterupted zeros (character break) or max tim length exceeded 
+
                 sig_cnt <= sig_cnt - 1;
                 
                 if inp = '0' then
@@ -107,6 +82,9 @@ begin
                 proc <= "110";
                 
               when 2 =>
+                -- turn LED off and wait for new recording start
+                -- let decoded character in 'tim' until new recording
+                -- TODO: join when 0 and when 2
                 proc <= "000";
                 if inp = '1' then
                     tim <= "1000000000000000000000";
@@ -124,6 +102,7 @@ begin
     
         end if; -- Rising edge
     else
+        -- reset state
         tim <= "0000000000000000000000";
         sig_state <= 0;
         proc <= "001";
--- labs/09-project/morse/morse.srcs/sources_1/new/top.vhd
+++ labs/09-project/morse/morse.srcs/sources_1/new/top.vhd
@@ -1,41 +1,14 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 26.04.2023 10:18:23
--- Design Name: 
--- Module Name: morsetop - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity top is
   port (
     CLK100MHZ   : in    std_logic;                    --! Main clock
     BTNR        : in    std_logic;                    --! High-active synchronous reset
-    BTNU        : in    std_logic; --! Traffic light for "south" direction
+    BTNU        : in    std_logic; 
     SWA         : in    std_logic_vector (5 downto 0);
     RGB         : out   std_logic_vector (2 downto 0);
     SPEAKER     : out   std_logic;
@@ -48,67 +21,72 @@ end top;
 
 architecture Behavioral of top is
 
-  -- Define the signal that uses different states
+  -- clock enable - sync signal with specific beat (one dit time unit)
   signal sig_ce : std_logic;
 
   -- Internal clock enable
   signal sig_en : std_logic;
 
-  -- Local delay counter
-  signal sig_tim : std_logic_vector(21 downto 0);
-  
-  
-  signal sig_tim_in : std_logic_vector(21 downto 0);
+  signal sig_out : std_logic;
 
+  signal sig_tim : std_logic_vector(21 downto 0);     -- timing to be transmited
+  signal sig_tim_in : std_logic_vector(21 downto 0);  -- recored timing
 
 begin
 
-
   clk_en0 : entity work.clock_enable
     generic map (
-      -- FOR SIMULATION, KEEP THIS VALUE TO 1
+      -- FOR SIMULATION - 1
       -- FOR IMPLEMENTATION, CALCULATE VALUE: 200 ms / (1/100 MHz)
       -- 1   @ 10 ns
       -- 20000000 @ 200 ms
       g_MAX => 20000000
     )
     port map (
-      clk => CLK100MHZ,
-      rst => BTNU,
+      clk => CLK100MHZ,   -- internal clock
+      rst => BTNU,        -- reset button
       ce  => sig_en
     );
-    
-   bin2morse_0 : entity work.bin2morse
+  
+  -- Trasmiter part
+
+  -- converts input from switch array to timing 
+  bin2morse_0 : entity work.bin2morse
     port map (
-      bin => SWA,
-      tim => sig_tim
+      bin => SWA,         -- binary input using switch array
+      tim => sig_tim      -- output timing (22bit vector)
     );
 
   morseout_0 : entity work.morseout
     port map (
-      clke => sig_en,
-      rst => BTNU,
-      start => BTNR,
+      clke => sig_en,  -- dit interval clock
+      rst => BTNU,     -- central reset button
+      start => BTNR,   -- start buuton
       tim => sig_tim,
-      proc => RGB,
-      outp => SPEAKER
+      proc => RGB,     -- state RGB led
+      outp => sig_out  -- output signal
     );
     
-    ---------
-    
+    -------------------------------------
+    -- Reciver part
+
     morse2bin_0 : entity work.morse2bin
     port map (
-      bin => LED_M,
-      tim => sig_tim_in
+      bin => LED_M,         -- output led array (binary representation of decoded character)
+      tim => sig_tim_in     -- recorded timing
     );
     
     morsein_0 : entity work.morsein
     port map (
       clke => sig_en,
-      rst => BTNU,
-      tim => sig_tim_in,
-      proc => RGB2,
-      inp => MORSE_PIN
+      rst => BTNU,        -- central reset button
+      tim => sig_tim_in,  -- recorded timing
+      proc => RGB2,       -- state RGB led
+      inp => MORSE_PIN    -- pin connected to morse signal (Arduino)
     );
+
+    -- connect output pin to both speaker (on expansion pin array) and internal LED
+    SPEAKER <= sig_out;
+    LED_OUT <= sig_out;
 
 end Behavioral;
